f3f9715e3be7144dd6b282b172ee7a7cfd79fc62
Lorelei Miyamura <lorelei.miyamura@gmail.com>
2020-02-23 21:23:05 -0600

    Updates

---
+++ precache-manifest.c090012a66715ffa72f19b1c7fe8a8b8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac66f9c9e4ecebebcc32a80db56d060f",
+    "url": "/ej-design-system/index.html"
+  },
+  {
+    "revision": "62b73fe15dfc32cbc3ee",
+    "url": "/ej-design-system/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "e4b45048538643a91cb9",
+    "url": "/ej-design-system/static/js/2.d42f06cb.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/ej-design-system/static/js/2.d42f06cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62b73fe15dfc32cbc3ee",
+    "url": "/ej-design-system/static/js/main.6b2d7bcb.chunk.js"
+  },
+  {
+    "revision": "bb6d954bbdd4ec0d1186",
+    "url": "/ej-design-system/static/js/runtime-main.dbb57189.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ej-design-system/static/media/logo.5d5d9eef.svg"
+  }
+]);
